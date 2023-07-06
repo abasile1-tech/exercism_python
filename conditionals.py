ea9d98efeddb7e6fda69fd0d67d0c0474cd71a81
John <ashton.basile@gmail.com>
2023-07-06 13:44:23 +0100

    my first solution for the conditionals exercise

--- conditionals.py
+++ conditionals.py
@@ -68,5 +68,10 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
 	2. 'NORMAL' -> `temperature * neutrons per second` +/- 10% of `threshold`
 	3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
 	"""
-
-	pass
+	multiplier = temperature * neutrons_produced_per_second
+	if multiplier < threshold * 0.9:
+		return 'LOW'
+	elif multiplier < threshold * 0.1 + threshold and multiplier > threshold - threshold * 0.1:
+		return 'NORMAL'
+	else:
+		return 'DANGER'
